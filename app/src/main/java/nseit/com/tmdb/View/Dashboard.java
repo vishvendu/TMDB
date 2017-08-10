499ce5ab3d415b041f297c4f9106554226c915c2
vishvendu <vishal.palawat785@gmail.com>
2017-08-10 18:42:51 +0530

    First commit add project into git

---
+++ app/src/main/java/nseit/com/tmdb/View/Dashboard.java
@@ -0,0 +1,15 @@
+package nseit.com.tmdb.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nseit.com.tmdb.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
